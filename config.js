bd1c74414555852c805e8fa0e03aed6cffee4748
Erika Pauwels <erika.pauwels@gmail.com>
2020-07-23 16:58:00 +0200

    Add sync of ingested documents at the end of sync task

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
-const INGEST_INTERVAL = process.env.INGEST_INTERVAL_MS || 300000; // 5 minutes TODO fix env var name
+const INGEST_INTERVAL = process.env.INGEST_INTERVAL || 300000; // 5 minutes TODO fix env var name
 const SYNC_BASE_URL = process.env.SYNC_BASE_URL || 'https://valvas-publications.vlaanderen.be';
 const SYNC_FILES_PATH = process.env.SYNC_FILES_PATH || '/publications';
 const SYNC_FILES_ENDPOINT = `${SYNC_BASE_URL}${SYNC_FILES_PATH}`;
 const DOWNLOAD_FILE_PATH = process.env.DOWNLOAD_FILE_PATH || '/files/:id/download';
 const DOWNLOAD_FILE_ENDPOINT = `${SYNC_BASE_URL}${DOWNLOAD_FILE_PATH}`;
+const DOWNLOAD_DOCUMENT_PATH = process.env.DOWNLOAD_DOCUMENT_PATH || '/kaleidos-files/:id/download';
+const DOWNLOAD_DOCUMENT_ENDPOINT = `${SYNC_BASE_URL}${DOWNLOAD_DOCUMENT_PATH}`;
 const MU_APPLICATION_GRAPH = 'http://mu.semte.ch/graphs/public';
 
 export {
@@ -11,5 +13,6 @@ export {
   SYNC_BASE_URL,
   SYNC_FILES_ENDPOINT,
   DOWNLOAD_FILE_ENDPOINT,
+  DOWNLOAD_DOCUMENT_ENDPOINT,
   MU_APPLICATION_GRAPH
 }
