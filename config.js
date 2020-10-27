be845f7cbe22f3362762bd7d2f03746550b9b77a
Erika Pauwels <erika.pauwels@gmail.com>
2020-10-27 09:02:38 +0100

    Update temporary import graph URI

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const DOWNLOAD_FILE_ENDPOINT = `${SYNC_BASE_URL}${DOWNLOAD_FILE_PATH}`;
 const DOWNLOAD_DOCUMENT_PATH = process.env.DOWNLOAD_DOCUMENT_PATH || '/kaleidos-files/:id/download';
 const DOWNLOAD_DOCUMENT_ENDPOINT = `${SYNC_BASE_URL}${DOWNLOAD_DOCUMENT_PATH}`;
 const MU_APPLICATION_GRAPH = 'http://mu.semte.ch/graphs/publication-tasks';
-const MU_TMP_BASE_GRAPH_URI = 'http://mu.semte.ch/imports/'
+const MU_TMP_BASE_GRAPH_URI = 'http://mu.semte.ch/graphs/import/';
 const BATCH_SIZE = parseInt(process.env.BATCH_SIZE) || 100;
 
 export {
