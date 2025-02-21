6490236d24376fb8ba07efabd068ae5e62b62ad7
Sven Valenberghs <sven.valenberghs@faros.be>
2025-02-21 15:31:21 +0100

    KAS-5010 add email on failure, WIP

--- config.js
+++ config.js
@@ -6,9 +6,17 @@ const DOWNLOAD_FILE_PATH = process.env.DOWNLOAD_FILE_PATH || '/files/:id/downloa
 const DOWNLOAD_FILE_ENDPOINT = `${SYNC_BASE_URL}${DOWNLOAD_FILE_PATH}`;
 const DOWNLOAD_DOCUMENT_PATH = process.env.DOWNLOAD_DOCUMENT_PATH || '/kaleidos-files/:id/download';
 const DOWNLOAD_DOCUMENT_ENDPOINT = `${SYNC_BASE_URL}${DOWNLOAD_DOCUMENT_PATH}`;
+const BATCH_SIZE = parseInt(process.env.BATCH_SIZE) || 100;
+const EMAIL_FROM_ADDRESS = process.env.EMAIL_FROM_ADDRESS ?? 'noreply@kaleidos.vlaanderen.be';
+const EMAIL_TO_ADDRESS_ON_FAILURE = process.env.EMAIL_TO_ADDRESS_ON_FAILURE ?? '';
+
+// constants
 const MU_APPLICATION_GRAPH = 'http://mu.semte.ch/graphs/publication-tasks';
 const MU_TMP_BASE_GRAPH_URI = 'http://mu.semte.ch/graphs/import/';
-const BATCH_SIZE = parseInt(process.env.BATCH_SIZE) || 100;
+const RELEASE_TASK_NOT_STARTED_STATUS = 'http://kanselarij.vo.data.gift/release-task-statuses/not-started';
+const RESOURCE_BASE_URI  = 'http://themis.vlaanderen.be';
+const EMAIL_GRAPH_URI = "http://mu.semte.ch/graphs/system/email";
+const EMAIL_OUTBOX_URI = "http://themis.vlaanderen.be/id/mail-folders/d9a415a4-b5e5-41d0-80ee-3f85d69e318c"
 
 export {
   INGEST_INTERVAL,
@@ -18,5 +26,11 @@ export {
   DOWNLOAD_DOCUMENT_ENDPOINT,
   MU_APPLICATION_GRAPH,
   MU_TMP_BASE_GRAPH_URI,
-  BATCH_SIZE
+  BATCH_SIZE,
+  EMAIL_FROM_ADDRESS,
+  EMAIL_TO_ADDRESS_ON_FAILURE,
+  RELEASE_TASK_NOT_STARTED_STATUS,
+  RESOURCE_BASE_URI,
+  EMAIL_GRAPH_URI,
+  EMAIL_OUTBOX_URI
 }
